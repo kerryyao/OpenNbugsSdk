aa0e8533fdab453b60caf833d87a031aaec23008
suguo.yao <yaokerry@gmail.com>
2016-09-22 23:14:04 +0800

    增加文件夹说明

--- OpenNbugsSdk/Config.cs
+++ OpenNbugsSdk/Config.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ONS
+﻿namespace ONS
 {
     /// <summary>
     /// 全局配置类
