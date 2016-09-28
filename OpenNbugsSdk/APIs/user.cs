95ac5e91edc2fa68386025d0f9fe027cab487434
Dong_PMer <1456547021@qq.com>
2016-09-28 12:14:54 +0800

    实现获取组织下人员的接口，双向认证的api预留，校园号未实现，所以没有实现。

---
+++ OpenNbugsSdk/APIs/user.cs
@@ -0,0 +1,22 @@
+﻿using ONS.Entities;
+using ONS.Utilities.HttpUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONS.APIs
+{
+    public class user
+    {
+        public Userinfo getUserinfo(string orgid, string userid, string oauth_token, bool with_contacts, bool with_orgext, bool with_card, bool with_roledetail)
+        {
+            Userinfo result = null;
+            string url = Config.URL_OPENBUGS + "/user/get?orgid=" + orgid + "&userid=" + userid + "&oauth_token=" + oauth_token;
+            var temp = Get.GetJson<RetResult<Userinfo>>(url);
+            result = temp.r;
+            return result;
+        }
+    }
+}
