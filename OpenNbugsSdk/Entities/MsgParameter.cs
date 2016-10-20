25a3bc167441822b5480d4ccde9cae8c822f8930
Dong_PMer <1456547021@qq.com>
2016-10-20 13:00:58 +0800

    完善部分注释

--- OpenNbugsSdk/Entities/MsgParameter.cs
+++ OpenNbugsSdk/Entities/MsgParameter.cs
@@ -12,28 +12,53 @@ namespace ONS.Entities
 
     public class MsgParameter
     {
+        /// <summary>
+        /// 消息编号，保证唯一
+        /// </summary>
         public string msgid { get; set; }
-
+        /// <summary>
+        /// 	接收消息的功能ID
+        /// </summary>
         public string funcid { get; set; }
-
+        /// <summary>
+        /// 消息类型，文本消息默认为0
+        /// </summary>
         public string msgtype { get; set; }
-
+        /// <summary>
+        /// 家长用户是接收消息，默认false
+        /// </summary>
         public string parentsReceive { get; set; }
-
+        /// <summary>
+        /// 学生本人是否接收消息，默认false
+        /// </summary>
         public string studentReceive { get; set; }
-
+        /// <summary>
+        /// 	用户所在组织机构ID
+        /// </summary>
         public string orgid { get; set; }
-
+        /// <summary>
+        /// 	发送者ID
+        /// </summary>
         public string senderid { get; set; }
-
+        /// <summary>
+        /// 	接收人ID，该字段与deptIds不能同时为空
+        /// </summary>
         public string userids { get; set; }
-
+        /// <summary>
+        /// 	接收部门ID，该字段与userIds不能同时为空
+        /// </summary>
         public string deptids { get; set; }
-
+        /// <summary>
+        /// 指定发送的用户角色，deptIds值不为空时有效
+        /// </summary>
         public string userroles { get; set; }
-
+        /// <summary>
+        /// 	指定不接收此消息的用户，deptIds值不为空时有效
+        /// </summary>
         public string exclude_userids { get; set; }
-
+        /// <summary>
+        /// 	是否对子部门下人员发送消息，deptIds值不为空时有效，默认false
+        /// </summary>
         public string with_subdepts { get; set; }
 
         public object msg { get; set; }
--- OpenNbugsSdk/Entities/Picture.cs
+++ OpenNbugsSdk/Entities/Picture.cs
@@ -8,16 +8,28 @@ namespace ONS.Entities
 {
     public class Picture
     {
+        /// <summary>
+        /// 图文消息的标题
+        /// </summary>
         public string title { get; set; }
-
+        /// <summary>
+        /// 	图文消息的摘要
+        /// </summary>
         public string summary { get; set; }
-
+        /// <summary>
+        /// 	图文消息的图片地址
+        /// </summary>
         public string picUrl { get; set; }
 
         public class link
         {
+            /// <summary>
+            /// 	图文消息打开时的回调地址ID
+            /// </summary>
             public string linkId { get; set; }
-
+            /// <summary>
+            /// 	图文消息打开后回调时的URL参数内容，平台会原样附加在回调URL中
+            /// </summary>
             public string urlParams { get; set; }
         }
     }
